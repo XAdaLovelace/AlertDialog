01b6da4efaf34e021eaddeaa9b5b2abef73d2ce6
Xadalovelace <wasalter101@gmail.com>
2018-12-02 00:23:45 +0200

    second

--- app/src/main/java/com/example/user/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/user/alertdialog/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import java.util.Random;
 import android.widget.Toast;
-
+//ghh
 
 public class MainActivity extends AppCompatActivity {
 
